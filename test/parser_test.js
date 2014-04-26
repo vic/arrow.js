09cbfa49812f97326f7b9e6b6d14fcee4a16c561
Victor Borja <vborja@apache.org>
2014-04-26 13:15:27 -0500

    Test parsing literals

--- test/parser_test.js
+++ test/parser_test.js
@@ -4,16 +4,40 @@ describe('Parser', function () {
 
   var Parser = require('../lib/parser')
   var Arrow  = require('../lib/arrow')
+  var ctx = {Arrow: Arrow}
 
-  it('tries to resolve an Arrow context', function (done) {
+  it('tries to resolve Arrow from context', function (done) {
     expect(function () { Parser('hello') }).to.throw(/Unknown word: Arrow/)
     done()
   })
 
-  it('resolves a word on given context objects', function (done) {
-    var p = Parser('hello', {hello: 'world', Arrow: Arrow})
+  it('resolves a word from given context objects', function (done) {
+    var p = Parser('hello', {hello: 'world'}, ctx)
     expect(p).to.equal('world')
     done()
+  })
+
+  describe('creates constant arrow for literal', function () {
+
+    var undefined
+    parseConstant('undefined', 'undefined', undefined)
+    parseConstant('null', 'null', null)
+    parseConstant('integers', '22', 22)
+    parseConstant('floats', '12.34', 12.34)
+    parseConstant('strings', '"hello"', "hello")
+
+
+    function parseConstant (desc, code, constant) {
+      it(desc, function (done) {
+        var arrow = Parser(code, ctx)
+        expect(arrow).to.satisfy(Arrow.isArrow)
+        arrow(function (x) {
+          expect(arguments).to.have.length(1)
+          expect(x).to.eql(constant)
+          done()
+        })
+      })
+    }
   })
 
 })
